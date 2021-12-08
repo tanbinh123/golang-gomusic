989f70243ec1c9cac2b959d7868ea64eecb379c5
dewble <dewble62@gmail.com>
2021-12-09 01:01:46 +0900

    사용자 로그인 정보 쿠키에 저장

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import About from './About';
 import Orders from './orders';
 // react-router-dom 패키지에서 필요한 컴포넌트 임포트
 import { BrowserRouter as Router, Route} from "react-router-dom";
+import cookie from 'js-cookie';
+
 
 
 
@@ -14,12 +16,18 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+    const user = cookie.getJSON("user") || {loggedin:false};
     this.state = {
-      user: {
-        loggedin: false,
-        name: ""
-      }
+      user: user,
+      showSignInModal: false,
+      showBuyModal: false
     };
+    this.handleSignedIn = this.handleSignedIn.bind(this);
+    this.handleSignedOut = this.handleSignedOut.bind(this);
+    this.showSignInModalWindow = this.showSignInModalWindow.bind(this);
+    this.showBuyModalWindow = this.showBuyModalWindow.bind(this);
+    this.toggleSignInModalWindow = this.toggleSignInModalWindow.bind(this);
+    this.toggleBuyModalWindow = this.toggleBuyModalWindow.bind(this);
   }
 
   handleSignedIn(user) {
@@ -28,15 +36,41 @@ class App extends React.Component {
     });
   }
 
-  componentDidMount() {
-    fetch('user.json')
-      .then(res => res.json())
-      .then((result) => {
-        console.log('Fetch...');
-        this.setState({
-          user: result
-        });
-      });
+  // 사용자가 로그아웃 했을 경우 쿠키에 해당 정보를 반영하여 사용자가 없음을 나타낸다
+  handleSignedOut(){
+    console.log("Call app signed out...");
+    const state = this.state;
+    const newState = Object.assign({},state,{user:{loggedin:false}});
+    this.setState(newState);
+    cookie.set("user",{loggedin:false});
+  }
+
+  // 구매와 로그인 모달 윈도우를 표시하는 Show 메서드
+  showSignInModalWindow(){
+    const state = this.state;
+    const newState = Object.assign({},state,{showSignInModal:true});
+    this.setState(newState);
+  }
+
+  showBuyModalWindow(id,price){
+    const state = this.state;
+    const newState = Object.assign({},state,{showBuyModal:true,productid:id,price:price});
+    this.setState(newState);
+  }
+
+  // 로그인과 구매 모달 윈도우의 toggle 메서드는 모달 윈도우의 출력 상태를 전환한다. 
+  // 따라서 모달 윈도우의 출력 여부를 나타내는 state 객체의 불리언 필드 값을 반전시키면 된다.
+
+  toggleSignInModalWindow() {
+    const state = this.state;
+    const newState = Object.assign({},state,{showSignInModal:!state.showSignInModal});
+    this.setState(newState);
+  }
+
+  toggleBuyModalWindow(){
+    const state = this.state;
+    const newState = Object.assign({},state,{showBuyModal:!state.showBuyModal});
+    this.setState(newState); 
   }
 
   render() {
